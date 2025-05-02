0649d97f465695f927e8acc67cef6725af3e3728
Guru Prasath <95870736+GURUPRASATH-OG@users.noreply.github.com>
2025-05-02 22:50:53 +0530

    Update UserService.js

--- src/service/UserService.js
+++ src/service/UserService.js
@@ -2,21 +2,21 @@ import axios from "axios";
 
 export const addUser = async (user) =>
 {
-    return await axios.post("http://localhost:8080/api/v1.0/admin/register",
+    return await axios.post("https://billingbackend-production-433c.up.railway.app/api/v1.0/admin/register",
         user,
         {headers:{'Authorization':`Bearer ${localStorage.getItem("token")}`}}
     )
 }
 export const deleteUser = async (userId)=>
 {
-    return await axios.delete(`http://localhost:8080/api/v1.0/admin/users/${userId}`,
+    return await axios.delete(`https://billingbackend-production-433c.up.railway.app/api/v1.0/admin/users/${userId}`,
         {headers:{'Authorization':`Bearer ${localStorage.getItem("token")}`}}
     )
 }
 
 export const fetchUsers = async () =>
 {
-    return await axios.get("http://localhost:8080/api/v1.0/admin/users",
+    return await axios.get("https://billingbackend-production-433c.up.railway.app/api/v1.0/admin/users",
         {headers:{'Authorization':`Bearer ${localStorage.getItem("token")}`}}
     )
 }
