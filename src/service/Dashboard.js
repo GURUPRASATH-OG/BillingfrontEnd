fc4c89c5b9e74476311dfd0b57ee68af2b331d34
Guru Prasath <95870736+GURUPRASATH-OG@users.noreply.github.com>
2025-05-02 22:47:10 +0530

    Update Dashboard.js

--- src/service/Dashboard.js
+++ src/service/Dashboard.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const fetchDashboardData =async () =>
 {
-   return await axios.get("http://localhost:8080/api/v1.0/dashboard",
+   return await axios.get("https://billingbackend-production-433c.up.railway.app/api/v1.0/dashboard",
         {headers:{'Authorization':`Bearer ${localStorage.getItem("token")} `}}
     )
 }
